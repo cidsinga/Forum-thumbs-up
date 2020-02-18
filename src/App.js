2260d3dcd48d978b13f50d5496d5fd454a87815d
Student <example@example.com>
2020-02-18 15:02:20 -0800

    create React app and add code to App.js and NewPost.jsx.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import NewPost from './components/NewPost.jsx';
+import Feed from './components/Feed.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterFeed: []
+    };
+    this.handleAddNewPostToFeed = this.handleAddNewPostToFeed.bind(this);
+  }
+
+  function handleAddNewPostToFeed(newPost) {
+    let newMasterFeed = this.state.masterFeed.slice();
+    newMasterFeed.push(newPost);
+    this.setState({masterFeed: newMasterFeed});
+  }
+
+  render() {
+    return (
+      <div>
+        <NewPost onNewPost={this.handleAddNewPostToFeed} />
+        <Feed feed={this.state.masterFeed} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NewPost.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 } from 'uuid';
+
+function NewPost(props) {
+  let _content = null;
+
+  function handleNewPostSubmission(event) {
+    event.preventDefault();
+    props.onNewPost({content: _content, id: v4()});
+    _content.value = '';
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleNewPostSubmission}>
+        <input
+        type='text'
+        id='content'
+        placeholder="What's on your mind?"
+        ref={(input) => {_content = input;}} />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+NewPost.propTypes = {
+  onNewPost: PropTypes.func
+};
+
+export default NewPost;
