cae98ffd0242139b4d3296bfad60b28006408e34
Student <example@example.com>
2020-02-18 16:23:10 -0800

    update Post.jsx to add counters.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Post(props) {
+class Post extends React.Component {
 
-  return (
-    <div>
-      <p>{props.content}</p>
-    </div>
-  )
+  constructor(props) {
+    super(props);
+    this.state = {
+      upVote: 0,
+      downVote: 0
+    };
+    this.handleUpVote = this.handleUpVote.bind(this);
+    this.handleDownVote = this.handleDownVote.bind(this);
+  }
+
+  handleUpVote(vote) {
+    let newUpVote = this.state.upVote + 1;
+    this.setState({upVote: newUpVote});
+  }
+
+  handleDownVote(vote) {
+    let newDownVote = this.state.downVote + 1;
+    this.setState({downVote: newDownVote});
+  }
+
+  render() {
+    const postStyle = {
+      display: 'flex',
+      justifyContent: 'space-around'
+    }
+    return (
+      <div style={postStyle}>
+      <p>{this.props.content}</p>
+      <p>{this.state.upVote}</p>
+      <p onClick={this.handleUpVote}><span role='img'>👍🏽</span></p>
+      <p>{this.state.downVote}</p>
+      <p onClick={this.handleDownVote}><span role='img'>👎🏽</span></p>
+      </div>
+    );
+  }
 }
 
 Post.propTypes = {
